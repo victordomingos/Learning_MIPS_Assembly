9e4a237b1042bb0cd8757c65c6f22a86954e8caa
Victor Domingos <editor.arcosonline@gmail.com>
2019-06-06 16:56:33 +0100

    02 square c++

---
+++ 02_simple_square.cpp
@@ -0,0 +1,16 @@
+// Calculate the square of a number (exercise: convert this to Assembly)
+// 2019-06-06 Victor Domingos
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 200;
+    int result;
+    
+    result = x * x;
+    cout << result;
+    return 0;
+}
